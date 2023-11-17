543188ac31c6f1757dd961db2d23d22ee16a758d
Alex Petit <alexpetit6@gmail.com>
2023-11-16 16:32:17 -0800

    added and styled contact bar

---
+++ src/components/ContactBar/ContactBar.jsx
@@ -0,0 +1,17 @@
+import './ContactBar.css';
+
+export default function ContactBar() {
+  return (
+    <div id='contact-bar'>
+      <a href="">
+        <img src="icons/insta-icon.png" alt="" />
+      </a>
+      <a href="">
+        <img src="icons/fbook-icon.png" alt="" />
+      </a>
+      <a href="">
+        <img id='email-icon' src="icons/email-icon.png" alt="" />
+      </a>
+    </div>
+  )
+}
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,14 @@
 import './Main.css';
 import Landing from '../../components/Landing/Landing';
+import ContactBar from '../../components/ContactBar/ContactBar';
 
 export default function Main () {
 
   return (
     <>
+    <ContactBar />
     <Landing />
     </>
   )
+
 }
